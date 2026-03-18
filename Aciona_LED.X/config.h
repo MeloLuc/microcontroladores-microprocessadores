0f5bada92770fba59448f4f0d0614ce7bb61de80
Lucas De Melo Monteiro Peixoto <lm159753456@gmail.com>
2026-03-17 23:01:33 -0300

    cleaning archives generated by MPLAB X and add .gitignore

---
+++ Aciona_LED.X/config.h
@@ -0,0 +1,26 @@
+
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+
+// CONFIG
+#pragma config FOSC = EXTRCCLK  // Oscillator Selection bits (RC oscillator: CLKOUT function on RA6/OSC2/CLKOUT pin, Resistor and Capacitor on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RA5/MCLR/VPP Pin Function Select bit (RA5/MCLR/VPP pin function is MCLR)
+#pragma config BOREN = ON       // Brown-out Detect Enable bit (BOD enabled)
+#pragma config LVP = ON         // Low-Voltage Programming Enable bit (RB4/PGM pin has PGM function, low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EE Memory Code Protection bit (Data memory code protection off)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+#define TRUE 1
+#define LIGA 1
+#define DESLIGA 0
+
+#define _XTAL_FREQ 8000000L // 8MHZ
+
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # microcontroladores-microprocessadores
-Repositório para armazenar conteúdo e códigos da matéria de microcontroladores e microprocessadores.
+Repositório para armazenar conteúdo e códigos da matéria de microcontroladores e microprocessadores ministrada pelo professor Mauro durante o 7° período no curso de engenharia da computação pela UEMG.
